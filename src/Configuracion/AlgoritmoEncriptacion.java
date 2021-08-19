899dc1236505ff5bcc392df4bb1283add3850d17
Cbermudez98 <cesar.bermudez.sierra@gmail.com>
2021-08-19 09:00:09 -0500

    second commit

---
+++ src/Configuracion/AlgoritmoEncriptacion.java
@@ -0,0 +1,66 @@
+package Configuracion;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.*;
+import javax.crypto.*;
+import javax.crypto.spec.DESKeySpec;
+
+public class AlgoritmoEncriptacion {
+
+    Cipher encrypt;
+    Cipher decrypt;
+    SecretKey key;
+    String keyTxt;
+
+    public AlgoritmoEncriptacion(String clave) {
+        keyTxt = clave;
+        try {
+            encrypt = Cipher.getInstance("DES");
+            decrypt = Cipher.getInstance("DES");
+
+            if (keyTxt.equals("")) {
+                SecretKey key = KeyGenerator.getInstance("DES").generateKey();//Genera Clave automàtica
+                encrypt.init(Cipher.ENCRYPT_MODE, key);//Con clave aleatoria
+                decrypt.init(Cipher.DECRYPT_MODE, key);//Con clave aleatoria
+            } else {
+                KeySpec ks = new DESKeySpec(keyTxt.getBytes("UTF8"));
+                SecretKeyFactory kf = SecretKeyFactory.getInstance("DES");
+                SecretKey ky = kf.generateSecret(ks);
+                encrypt.init(Cipher.ENCRYPT_MODE, ky);
+                decrypt.init(Cipher.DECRYPT_MODE, ky);
+            }
+        } catch (InvalidKeySpecException ex) {
+        } catch (UnsupportedEncodingException ex) {
+        } catch (InvalidKeyException ex) {
+        } catch (NoSuchAlgorithmException ex) {
+        } catch (NoSuchPaddingException ex) {
+        }
+    }
+
+    public String encriptar(String str) {
+        try {
+            byte[] utf8 = str.getBytes("UTF8");
+            byte[] enc = encrypt.doFinal(utf8);
+            return new sun.misc.BASE64Encoder().encode(enc);
+        } catch (javax.crypto.BadPaddingException e) {
+        } catch (IllegalBlockSizeException e) {
+        } catch (UnsupportedEncodingException e) {
+        } catch (java.io.IOException e) {
+        }
+        return null;
+    }
+
+    public String desencriptar(String str) {
+        try {
+            byte[] dec = new sun.misc.BASE64Decoder().decodeBuffer(str);
+            byte[] utf8 = decrypt.doFinal(dec);
+            return new String(utf8, "UTF8");
+        } catch (javax.crypto.BadPaddingException e) {
+        } catch (IllegalBlockSizeException e) {
+        } catch (UnsupportedEncodingException e) {
+        } catch (java.io.IOException e) {
+        }
+        return null;
+    }
+}
---
+++ src/conversosbase64/ConversosBase64.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conversosbase64;
+
+import Vista.VistraPrincipal;
+import com.formdev.flatlaf.intellijthemes.FlatArcIJTheme;
+import java.awt.Color;
+import java.awt.Insets;
+import javax.swing.UIManager;
+
+/**
+ *
+ * @author Auxiliar
+ */
+public class ConversosBase64 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        try {
+            UIManager.setLookAndFeel(new FlatArcIJTheme());
+            UIManager.put("Component.arrowType", "chevron");
+            UIManager.put("ScrollBar.trackArc", 999);
+            UIManager.put("ScrollBar.thumbArc", 999);
+            UIManager.put( "TextComponent.arc", 999 );
+            UIManager.put("ScrollBar.trackInsets", new Insets(2, 4, 2, 4));
+            UIManager.put("ScrollBar.thumbInsets", new Insets(2, 2, 2, 2));
+            UIManager.put("ScrollBar.track", new Color(0xe0e0e0));
+            UIManager.put("Button.arc", 999);
+            UIManager.put("TabbedPane.selectedBackground", Color.white);
+        } catch (Exception ex) {
+            System.err.println("Failed to initialize LaF");
+        }
+        Vista.VistraPrincipal vp = new VistraPrincipal();
+        vp.setVisible(true);
+    }
+    
+}
